cd9317485b4adb79ce9caa2274e7db117c9c49e0
Michel Christophe <mchristophecoach@gmail.com>
2025-11-03 17:46:02 +0100

    ADD: passprint sdk with node:crypto changed by react-native-quick-crypto

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,149 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { install } from 'react-native-quick-crypto';
+
+
+import { Image } from 'expo-image';
+import { StyleSheet } from 'react-native';
+
+import ParallaxScrollView from '@/components/parallax-scroll-view';
+import { ThemedText } from '@/components/themed-text';
+import { ThemedView } from '@/components/themed-view';
+import { passprintService } from '@/preparation/sdk/backend/PassprintUtils';
+import { useEffect, useState } from 'react';
+import * as Keychain from 'react-native-keychain';
+install();
+export default function HomeScreen() {
+console.log("on affiche layout")
+  const [isTheFirstTime, setIsTheFirstTime] = useState<undefined | boolean>(undefined);
+
+
+  useEffect(()=>{
+    const doesUserAlreadyExiste = async () =>{
+      const alreadyExist = await passprintService.areKeysAlreadyExisting();
+      setIsTheFirstTime(alreadyExist);
+    } 
+    doesUserAlreadyExiste();
+  
+ 
+  }, [])
+  
+ 
+
+  return (
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+      headerImage={
+        <Image
+          source={require('@/assets/images/partial-react-logo.png')}
+          style={styles.reactLogo}
+        />
+      }>
+      <ThemedView style={styles.titleContainer}>
+        {
+          isTheFirstTime === undefined ?
+          <ThemedText type="title">Vérification de votre identité</ThemedText>
+          :
+          isTheFirstTime ?
+          <ThemedText type="title">C'est la première fois que vous utilisez l'application</ThemedText>
+          :
+          <ThemedText type="title">Vous avez déjà utilisé l'application</ThemedText>
+
+        }
+        
+           
+              
+      </ThemedView>
+      
+    </ParallaxScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
+  },
+});
+
+
+
+
+const KEYCHAIN_SERVICE = 'com.passprint.cryptokeys';
+
+export const getStoredSecretsSecured = async () => {
+    try {
+        const credentials = await Keychain.getGenericPassword({
+            service: KEYCHAIN_SERVICE,
+            
+            // 🛑 C'est le paramètre CRITIQUE 🛑
+            accessControl: Keychain.ACCESS_CONTROL.BIOMETRY_ANY,             
+            
+            authenticationPrompt: {
+                title: 'Vérification Passprint',
+                subtitle: 'Authentification requise pour déverrouiller vos clés cryptographiques',
+            },
+        });
+
+        if (!credentials) {
+           throw new Error("no credentials found.")
+        }
+        return JSON.parse(credentials.password);
+
+    } catch (error) {
+        // L'erreur ici signifie généralement que l'utilisateur a annulé ou que la biométrie a échoué.
+        console.error("Échec de l'accès biométrique ou annulation :", error);
+        return null;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
@@ -15,10 +160,11 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
+        <Stack.Screen name="ensureDeviceEnrollment" options={{}} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
   );
-}
+}*/
--- eslint.config.js
+++ eslint.config.js
@@ -6,5 +6,9 @@ module.exports = defineConfig([
   expoConfig,
   {
     ignores: ['dist/*'],
+    rules: {
+      // Désactiver la règle qui empêche d'utiliser l'apostrophe (') directement dans le JSX
+      'react/no-unescaped-entities': 'off',
+    },
   },
 ]);
