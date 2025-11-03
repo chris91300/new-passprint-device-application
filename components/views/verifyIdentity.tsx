f6310b7fc84a1c1ae18b795c736f57cd55c9fccb
Michel Christophe <mchristophecoach@gmail.com>
2025-11-03 18:32:31 +0100

    ADD: hook useIsFirstTime. check if there is a credential

---
+++ components/views/verifyIdentity.tsx
@@ -0,0 +1,12 @@
+import { ThemedText } from '@/components/themed-text';
+
+
+function VerifyIdentity() {
+  return (
+    <ThemedText type="title">
+      Vérification de votre identité
+    </ThemedText>
+  )
+}
+
+export default VerifyIdentity
